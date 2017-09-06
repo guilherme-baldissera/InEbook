65a787789351fbf6c071d36657ff233d8156e54a
guilherme250493 <guilherme250493@hotmail.com>
2017-09-06 11:49:21 -0300

    cadastro e delete

--- InEBook/src/java/com/indra/inebook/controller/managerbeans/LivroManagerBean.java
+++ InEBook/src/java/com/indra/inebook/controller/managerbeans/LivroManagerBean.java
@@ -11,9 +11,11 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -29,20 +31,55 @@ public class LivroManagerBean implements Serializable {
     private String consultaName;
     private String tipoConsulta;
 
+    //livros atributos
+    private String nomeLivro;
+    private String autorLivro;
+    private Float precoLivro;
+    private Integer quantidadeLivro;
+    private String seccaoLivro;
+
     @PostConstruct
     public void init() {
         this.livros = new ArrayList<Livro>();
+        this.livroSelected = null;
+    }
 
+    public void deletarLivro(){
+        PostgresLivroDAO pldao = new PostgresLivroDAO();
+        
+        if(livroSelected == null){
+            errorDelete();
+        }
+        else{
+           pldao.deleteLivroEstByID(livroSelected.getId());
+           pldao.deleteLivroRegByID(livroSelected.getId());
+           buscarLivros();
+           infoDelete();
+        }
+    }
+    
+    public void novoLivro() {
+        PostgresLivroDAO pldao = new PostgresLivroDAO();
+        Integer id;
+        if (pldao.verifyIfExistLivro(nomeLivro.toUpperCase(), autorLivro.toUpperCase())) {
+            error();
+        } else {
+            pldao.insertLivroReg(nomeLivro.toUpperCase(), autorLivro.toUpperCase());
+            id = pldao.getIdByNomeAutor(nomeLivro.toUpperCase(), autorLivro.toUpperCase());
+            pldao.insertLivroEst(quantidadeLivro, seccaoLivro.toUpperCase(), id, precoLivro);
+            info();
+        }
     }
 
     public void buscarLivros() {
 
         PostgresLivroDAO pldao = new PostgresLivroDAO();
-        if(this.tipoConsulta.equals("1"))
-            livros = pldao.getLivroByNome(this.consultaName);
-        else if(this.tipoConsulta.equals("2"))
-            livros = pldao.getLivroByAutor(this.consultaName);
-        
+        if (this.tipoConsulta.equals("1")) {
+            livros = pldao.getLivroByNome(this.consultaName.toUpperCase());
+        } else if (this.tipoConsulta.equals("2")) {
+            livros = pldao.getLivroByAutor(this.consultaName.toUpperCase());
+        }
+
     }
 
     public List<Livro> getLivros() {
@@ -76,6 +113,59 @@ public class LivroManagerBean implements Serializable {
     public void setLivroSelected(Livro livroSelected) {
         this.livroSelected = livroSelected;
     }
-    
 
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+
+    public String getAutorLivro() {
+        return autorLivro;
+    }
+
+    public void setAutorLivro(String autorLivro) {
+        this.autorLivro = autorLivro;
+    }
+
+    public Float getPrecoLivro() {
+        return precoLivro;
+    }
+
+    public void setPrecoLivro(Float precoLivro) {
+        this.precoLivro = precoLivro;
+    }
+
+    public Integer getQuantidadeLivro() {
+        return quantidadeLivro;
+    }
+
+    public void setQuantidadeLivro(Integer quantidadeLivro) {
+        this.quantidadeLivro = quantidadeLivro;
+    }
+
+    public String getSeccaoLivro() {
+        return seccaoLivro;
+    }
+
+    public void setSeccaoLivro(String seccaoLivro) {
+        this.seccaoLivro = seccaoLivro;
+    }
+
+    public void info() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Cadastrado com Sucesso."));
+    }
+
+    public void error() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Livro Ja Cadastrado"));
+    }
+    public void infoDelete() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Deletado com sucesso"));
+    }
+
+    public void errorDelete() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Não foi selecionado nenhum livro."));
+    }
 }
--- InEBook/src/java/com/indra/inebook/model/dao/PostgresLivroDAO.java
+++ InEBook/src/java/com/indra/inebook/model/dao/PostgresLivroDAO.java
@@ -1,6 +1,5 @@
 package com.indra.inebook.model.dao;
 
-
 import com.indra.inebook.model.vo.Livro;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,64 +19,229 @@ public class PostgresLivroDAO implements LivroDAO {
     private static final String DBURL = "jdbc:postgresql://LOCALHOST:5432/InEbook";
     private static final String USER = "postgres";
     private static final String SENHA = "25041993";
-    
+    //selecao
     private static final String SQL_LIVROS_BY_NOME = "SELECT LR.ID,LR.NOME,LR.AUTOR,LE.PRECO,LE.SECCAO,LE.QUANTIDADE FROM LIVROS_REGISTRADOS AS LR, LIVROS_ESTOQUE AS LE WHERE LR.ID = LE.ID_REG AND LR.NOME LIKE ?";
     private static final String SQL_LIVROS_BY_AUTOR = "SELECT LR.ID,LR.NOME,LR.AUTOR,LE.PRECO,LE.SECCAO,LE.QUANTIDADE FROM LIVROS_REGISTRADOS AS LR, LIVROS_ESTOQUE AS LE WHERE LR.ID = LE.ID_REG AND LR.AUTOR LIKE ?";
+    private static final String SQL_GETID_BY_NOME_AUTOR = "SELECT ID FROM LIVROS_REGISTRADOS WHERE NOME = ? AND AUTOR = ?";
+    //insert
+    private static final String SQL_INSERT_LIVROREG = "INSERT INTO LIVROS_REGISTRADOS(NOME,AUTOR) VALUES(?,?)";
+    private static final String SQL_INSERT_LIVROEST = "INSERT INTO LIVROS_ESTOQUE(QUANTIDADE,SECCAO,ID_REG,PRECO) VALUES(?,?,?,?)";
+    //delete
+    private static final String SQL_DELETE_LIVROREG_BY_ID = "DELETE FROM LIVROS_REGISTRADOS WHERE ID = ?";
+    private static final String SQL_INSERT_LIVROEST_BY_ID = "DELETE FROM LIVROS_ESTOQUE WHERE ID = ?";
+    
+    public Boolean deleteLivroRegByID(Integer id){
+        Boolean value = false;
 
+        try (Connection cnn = getConnection()) {
+            try (PreparedStatement ps = cnn.prepareStatement(SQL_DELETE_LIVROREG_BY_ID)) {
+                ps.setInt(1, id);
+
+                ps.executeUpdate();
+                value = true;
+
+            } catch (SQLException sql) {
+                System.out.println(sql.getMessage());
+                sql.printStackTrace(System.err);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace(System.err);
+
+        }
+
+        return value;
+    }
+    public Boolean deleteLivroEstByID(Integer id){
+        Boolean value = false;
+
+        try (Connection cnn = getConnection()) {
+            try (PreparedStatement ps = cnn.prepareStatement(SQL_INSERT_LIVROEST_BY_ID)) {
+                ps.setInt(1, id);
+
+                ps.executeUpdate();
+                value = true;
+
+            } catch (SQLException sql) {
+                System.out.println(sql.getMessage());
+                sql.printStackTrace(System.err);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace(System.err);
+
+        }
+
+        return value;
+    }
     
     
+    public Boolean insertLivroReg(String nome, String autor) {
+        Boolean value = false;
+
+        try (Connection cnn = getConnection()) {
+            try (PreparedStatement ps = cnn.prepareStatement(SQL_INSERT_LIVROREG)) {
+                ps.setString(1, nome);
+                ps.setString(2, autor);
+
+                ps.executeUpdate();
+                value = true;
+
+            } catch (SQLException sql) {
+                System.out.println(sql.getMessage());
+                sql.printStackTrace(System.err);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace(System.err);
+
+        }
+
+        return value;
+    }
+
+    public Boolean insertLivroEst(Integer quantidade, String seccao, Integer id_reg, Float preco) {
+        Boolean value = false;
+
+        try (Connection cnn = getConnection()) {
+            try (PreparedStatement ps = cnn.prepareStatement(SQL_INSERT_LIVROEST)) {
+                ps.setInt(1, quantidade);
+                ps.setString(2, seccao);
+                ps.setInt(3, id_reg);
+                ps.setFloat(4, preco);
+
+                ps.executeUpdate();
+                value = true;
+
+            } catch (SQLException sql) {
+                System.out.println(sql.getMessage());
+                sql.printStackTrace(System.err);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace(System.err);
+
+        }
+
+        return value;
+    }
+
+    public Boolean verifyIfExistLivro(String nome, String autor) {
+        Boolean value = false;
+
+        try (Connection cnn = getConnection()) {
+            try (PreparedStatement ps = cnn.prepareStatement(SQL_GETID_BY_NOME_AUTOR)) {
+                ps.setString(1, nome);
+                ps.setString(2, autor);
+
+                try (ResultSet rs = ps.executeQuery()) {
+                    value = rs.next();
+                } catch (SQLException sql) {
+                    System.out.println(sql.getMessage());
+                    sql.printStackTrace(System.err);
+                }
+
+            } catch (SQLException sql) {
+                System.out.println(sql.getMessage());
+                sql.printStackTrace(System.err);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace(System.err);
+
+        }
+
+        return value;
+    }
+
+    public Integer getIdByNomeAutor(String nome, String autor) {
+        Integer value = 0;
+
+        try (Connection cnn = getConnection()) {
+            try (PreparedStatement ps = cnn.prepareStatement(SQL_GETID_BY_NOME_AUTOR)) {
+                ps.setString(1, nome);
+                ps.setString(2, autor);
+
+                try (ResultSet rs = ps.executeQuery()) {
+                    while(rs.next()){
+                        value = rs.getInt(1);
+                    }
+                } catch (SQLException sql) {
+                    System.out.println(sql.getMessage());
+                    sql.printStackTrace(System.err);
+                }
+
+            } catch (SQLException sql) {
+                System.out.println(sql.getMessage());
+                sql.printStackTrace(System.err);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace(System.err);
+
+        }
+
+        return value;
+    }
+
     public List<Livro> getLivroByNome(String livro) {
         List<Livro> livros = new ArrayList<Livro>();
-        try(Connection cnn = getConnection()){
-            try(PreparedStatement ps = cnn.prepareStatement(SQL_LIVROS_BY_NOME)){
-            ps.setString(1, (livro+"%"));
-                try (ResultSet rs = ps.executeQuery()){
-                    while(rs.next()){
-                        livros.add(new Livro(rs.getInt(1), rs.getString(2), rs.getString(3),rs.getFloat(4), rs.getString(5),rs.getInt(6)));
-                    }      
+        try (Connection cnn = getConnection()) {
+            try (PreparedStatement ps = cnn.prepareStatement(SQL_LIVROS_BY_NOME)) {
+                ps.setString(1, (livro + "%"));
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        livros.add(new Livro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getFloat(4), rs.getString(5), rs.getInt(6)));
+                    }
                 } catch (SQLException sql) {
                     System.out.println(sql.getMessage());
                     sql.printStackTrace(System.err);
                 }
-            }catch(SQLException sql){
+            } catch (SQLException sql) {
                 System.out.println(sql.getMessage());
                 sql.printStackTrace(System.err);
             }
-          
-        }catch(Exception e){
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace(System.err);
-            
+
         }
-            return livros;
-        }
+        return livros;
+    }
+
     public List<Livro> getLivroByAutor(String autor) {
         List<Livro> livros = new ArrayList<Livro>();
-        try(Connection cnn = getConnection()){
-            try(PreparedStatement ps = cnn.prepareStatement(SQL_LIVROS_BY_AUTOR)){
-            ps.setString(1, (autor+"%"));
-                try (ResultSet rs = ps.executeQuery()){
-                    while(rs.next()){
-                        livros.add(new Livro(rs.getInt(1), rs.getString(2), rs.getString(3),rs.getFloat(4), rs.getString(5),rs.getInt(6)));
-                    }      
+        try (Connection cnn = getConnection()) {
+            try (PreparedStatement ps = cnn.prepareStatement(SQL_LIVROS_BY_AUTOR)) {
+                ps.setString(1, (autor + "%"));
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        livros.add(new Livro(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getFloat(4), rs.getString(5), rs.getInt(6)));
+                    }
                 } catch (SQLException sql) {
                     System.out.println(sql.getMessage());
                     sql.printStackTrace(System.err);
                 }
-            }catch(SQLException sql){
+            } catch (SQLException sql) {
                 System.out.println(sql.getMessage());
                 sql.printStackTrace(System.err);
             }
-          
-        }catch(Exception e){
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace(System.err);
-            
+
         }
 
-
-            return livros;
-        }
+        return livros;
+    }
 
     public Connection getConnection() {
         Connection conn = null;
